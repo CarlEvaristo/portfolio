6b344ffd737c56c1b15ba1abfa140eac3e429dea
CarlEverest78 <thepythoneer@gmail.com>
2022-10-04 15:29:43 +0200

    add contact icon links: linkedin and github

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import "./Footer.css"
-import TwitterIcon from '@mui/icons-material/Twitter';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import EmailIcon from '@mui/icons-material/Email';
 import GitHubIcon from '@mui/icons-material/GitHub';
